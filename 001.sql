4990fcaf2ac56c768bf0b0ff481f78e0ae8dba6b
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2025-02-26 12:17:33 +0000

    Atualizar o

---
+++ 001.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tipos_ataques(
+    nome VARCHAR(50),
+    tipo VARCHAR(50),
+    id TINYINT(1),
+    consequencias VARCHAR(250),
+;)
+INSERT INTO
+tipos_ataques(id,nome,tipo,consequencias)VALUES
+(1, phishing,engenharia social,roubo de informacoes),
+(2,DDoS,sobrecarga,indisponibilidade de algo),
+(3, Ransomware,engenharia socia,roubo de informacoes),
+(4,Malware,virus,roubo de informacoes e derrubar um sistema);
+SELECT * FROM tipos_ataques;
