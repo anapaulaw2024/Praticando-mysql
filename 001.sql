70e842cba059d1f6570273528534be08980dc17a
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2025-02-26 13:05:40 +0000

    Atualizar o 001.sql

--- 001.sql
+++ 001.sql
@@ -6,8 +6,8 @@ CREATE TABLE tipos_ataques(
 ;)
 INSERT INTO
 tipos_ataques(id,nome,tipo,consequencias)VALUES
-(1, phishing,engenharia social,roubo de informacoes),
-(2,DDoS,sobrecarga,indisponibilidade de algo),
-(3, Ransomware,engenharia socia,roubo de informacoes),
-(4,Malware,virus,roubo de informacoes e derrubar um sistema);
+(1, 'phishing','engenharia social','roubo de informacoes)',
+(2,'DDoS','sobrecarga','indisponibilidade de algo'),
+(3, 'Ransomware','engenharia social','roubo de informacoes'),
+(4,'Malware','virus','roubo de informacoes e derrubar um sistema');
 SELECT * FROM tipos_ataques;
